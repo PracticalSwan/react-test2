a048992a4cd956f476fd6a9594694c9f54ddce35
Sithu <u6726077@au.edu>
2026-01-04 23:44:35 +0700

    Built a Card Component

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+    return(
+        <div className="card">
+            <img className="card-image" src="https://picsum.photos/150" alt="profile picture"></img>
+            <h2 className="card-title">Sithu Win San</h2>
+            <p className="card-text">I study Computer Science and play video games</p>
+        </div>
+    );
+}
+
+export default Card
